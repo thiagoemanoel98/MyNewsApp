a5191e788390f206af94feb36ed21cf383ba088d
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-08-16 10:50:05 -0300

    Feat: loading view

--- MyNewsApp/Controllers/NewsViewController.swift
+++ MyNewsApp/Controllers/NewsViewController.swift
@@ -11,7 +11,8 @@ class NewsViewController: UIViewController {
     
     // Conecta o interface builder com o interface component
     @IBOutlet weak var newsWebView:WKWebView!
-
+    @IBOutlet weak var loadingView: UIView!;
+    @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
     
     var news: NewsModel? {
         didSet {
@@ -26,10 +27,32 @@ class NewsViewController: UIViewController {
     }
     
     private func setupWebView() {
+        self.newsWebView.navigationDelegate = self;
+        
         guard let news = news, let url = URL(string: news.url) else { return };
         
         self.newsWebView.load(URLRequest(url: url));
+        self.newsWebView.allowsBackForwardNavigationGestures = true;
         
     }
 
+}
+
+extension NewsViewController: WKNavigationDelegate {
+    // ver navegacao da webview
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        self.loadingActivityIndicator.startAnimating();
+        self.loadingView.isHidden = false;
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.loadingView.isHidden = true;
+        self.loadingActivityIndicator.stopAnimating();
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.loadingView.isHidden = true;
+        self.loadingActivityIndicator.stopAnimating();
+    }
 }
